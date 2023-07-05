dde809264522a6b7c7cb4e055d9f77de98a23d0f
risenfromashes <ashrafurkhan37@gmail.com>
2023-07-06 04:10:47 +0600

    make changes

--- setup_ext.py
+++ setup_ext.py
@@ -59,7 +59,7 @@ include_dirs = [extensions_dir]
 
 ext_modules = [
     extension(
-        "detrexc",
+        "detrex._C",
         sources,
         include_dirs=include_dirs,
         define_macros=define_macros,
@@ -68,7 +68,7 @@ ext_modules = [
 ]
 
 setup(
-    name="detrexc",
+    name="detrex._C",
     ext_modules=ext_modules,
     cmdclass={'build_ext': torch.utils.cpp_extension.BuildExtension}
 )
